df100a72c4fe539df0d01ed3b79d20758128146f
tanpham <phamtan500@gmail.com>
2018-07-14 00:38:11 +0700

    function component

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:"Max", age:28 },
+      { name:"Manu", age:29 },
+      { name:"Stephannie", age:26 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked');
+    this.setState({
+      persons: [
+        { name:"Maximilian", age:28 },
+        { name:"Manu", age:29 },
+        { name:"Stephannie", age:26 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1> hello world </h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing </Person> 
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }

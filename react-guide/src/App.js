f0e8701b5cd07ea833810e5a0d5018c907269771
tanpham <phamtan500@gmail.com>
2018-07-15 15:19:33 +0700

    finish of 4

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -2,33 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
+
 class App extends Component {
+  // state and data of the app
   state = {
     persons: [
-      { name:"Max", age:28 },
-      { name:"Manu", age:29 },
-      { name:"Stephannie", age:26 }
-    ]
+      { id:'aeb', name:"Manu", age:29 },
+      { id:'ghk', name:"Max", age:28 },
+      { id:'12d', name:"Stephannie", age:26 }
+    ],
+    showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name:newName, age:28 },
-        { name:"Manu", age:29 },
-        { name:"Stephannie", age:26 }
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    // spread operator
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age:28 },
-        { name: event.target.value, age:29 },
-        { name: "Stephannie", age:26 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    // find index with id
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // copy the object by spearding operator
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // reset name
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    // reset state
+    this.setState( {persons: persons} );
+  }
+
+  // change state when click to button
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    // change part of state
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -41,25 +60,35 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    // reder content with condition
+    if (this.state.showPersons)
+    {
+      // assign to JSX
+      persons = (
+        <div>
+          {/* map one person data to one person jsx */}
+          {this.state.persons.map( (person, index) => {
+            return <Person 
+            // handler with parameter
+            click={() => this.deletePersonHandler(index)} 
+            name={person.name} 
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event,person.id)}/>
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <h1> hello world </h1>
+        <h1> This is really working! </h1>
         <button 
           style={style}
-          onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
-
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}/>
-        
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        changed={this.nameChangedHandler}>My Hobbies: Racing </Person> 
-        
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age} />
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   }
--- react-guide/src/Person/Person.js
+++ react-guide/src/Person/Person.js
@@ -1,14 +1,23 @@
+// always import React inside component
 import React from 'react';
+// import component css
 import Person from './Person.css';
 
+// component communicate with app by props
 const person = (props) => {
+
+  // return jsx inside ()
   return (
+    // set style with className
     <div className="Person">
+      {/* event handle with onClick */}
       <p onClick={props.click}> I'm a {props.name} and I am {props.age} years old !</p>
       <p> {props.children} </p>
+      {/* set property by using props */}
       <input type="text" onChange={props.changed} value={props.name}/>
     </div>
   )
 }
 
+// export component to outside
 export default person;
